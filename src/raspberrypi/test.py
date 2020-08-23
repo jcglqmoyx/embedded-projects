eeebe4af7516171a6cd5a3f0713465a08125bf5b
Chintsai Hwo <jcglqmoyx@gmail.com>
2020-08-23 22:56:52 +0800

    Initial commit

---
+++ src/raspberrypi/test.py
@@ -0,0 +1,27 @@
+import RPi.GPIO
+import time
+RPi.GPIO.setwarnings(False)
+RPi.GPIO.setmode(RPi.GPIO.BOARD)
+RPi.GPIO.setup(11, RPi.GPIO.OUT)
+RPi.GPIO.setup(12, RPi.GPIO.OUT)
+RPi.GPIO.setup(13, RPi.GPIO.OUT)
+i = 1
+while True:
+    RPi.GPIO.output(11, 1)
+    print('red\n')
+    time.sleep(3)
+    RPi.GPIO.output(12, 1)
+    print('green\n')
+    time.sleep(3)
+    RPi.GPIO.output(13, 1)
+    print('blue\n')
+    time.sleep(3)
+    RPi.GPIO.output(11, 0)
+    RPi.GPIO.output(12, 0)
+    RPi.GPIO.output(13, 0)
+    time.sleep(0.2)
+    i += 1
+    if i == 5:
+        break
+
+RPi.GPIO.cleanup()
