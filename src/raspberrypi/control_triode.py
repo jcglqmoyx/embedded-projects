00e3bb98eb75d74cada4eba57c936d813ad629ac
Chintsai Hwo <jcglqmoyx@gmail.com>
2020-08-23 23:03:44 +0800

    update

--- src/raspberrypi/control_triode.py
+++ src/raspberrypi/control_triode.py
@@ -9,17 +9,19 @@ i = 1
 while True:
     RPi.GPIO.output(11, 1)
     print('red\n')
-    time.sleep(3)
+    time.sleep(2)
+    RPi.GPIO.output(11, 0)
+
     RPi.GPIO.output(12, 1)
     print('green\n')
-    time.sleep(3)
+    time.sleep(2)
+    RPi.GPIO.output(12, 0)
+
     RPi.GPIO.output(13, 1)
     print('blue\n')
-    time.sleep(3)
-    RPi.GPIO.output(11, 0)
-    RPi.GPIO.output(12, 0)
+    time.sleep(2)
     RPi.GPIO.output(13, 0)
-    time.sleep(0.2)
+
     i += 1
     if i == 5:
         break
