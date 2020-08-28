54e2d8f98b0370c74d7438aa9d4586eda3c8f5fb
Chintsai Hwo <jcglqmoyx@gmail.com>
2020-08-28 22:09:57 +0800

    update

--- src/raspberrypi/light_up_diode.py
+++ src/raspberrypi/light_up_diode.py
@@ -7,18 +7,17 @@ RPi.GPIO.setmode(RPi.GPIO.BOARD)
 
 RPi.GPIO.setup(11, RPi.GPIO.OUT)
 RPi.GPIO.setup(12, RPi.GPIO.OUT)
-RPi.GPIO.setup(13, RPi.GPIO.OUT)
 
 i = 1
 while True:
     RPi.GPIO.output(12, 1)
     print('red\n')
-    time.sleep(2)
+    time.sleep(0.2)
     RPi.GPIO.output(12, 0)
 
     RPi.GPIO.output(11, 1)
     print('green\n')
-    time.sleep(2)
+    time.sleep(0.2)
     RPi.GPIO.output(11, 0)
 
     i += 1
