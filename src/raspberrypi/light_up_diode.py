046753e65229b9123d30ecb16301274de7cb7eb2
Chintsai <jcglqmoyx@gmail.com>
2020-12-21 17:02:00 +0800

    update

--- src/raspberrypi/light_up_diode.py
+++ src/raspberrypi/light_up_diode.py
@@ -1,8 +1,15 @@
-import RPi.GPIO
+import datetime
 import time
 
-RPi.GPIO.setwarnings(False)
+import RPi.GPIO
 
+current_date = datetime.datetime.now()
+hour = current_date.hour
+ur = hour % 12
+minute = current_date.minute
+print(current_date)
+
+RPi.GPIO.setwarnings(False)
 RPi.GPIO.setmode(RPi.GPIO.BOARD)
 
 RPi.GPIO.setup(11, RPi.GPIO.OUT)
@@ -18,21 +25,29 @@ RPi.GPIO.setup(37, RPi.GPIO.OUT)
 
 i = 1
 while True:
-    RPi.GPIO.output(11, 1)
-    time.sleep(0.2)
-    RPi.GPIO.output(11, 0)
+    hour -= 1
+    if hour >= 0:
+        RPi.GPIO.output(15, 1)
+    else:
+        RPi.GPIO.output(15, 0)
 
-    RPi.GPIO.output(12, 1)
-    time.sleep(0.2)
-    RPi.GPIO.output(12, 0)
+    hour -= 2
+    if hour >= 0:
+        RPi.GPIO.output(13, 1)
+    else:
+        RPi.GPIO.output(13, 0)
 
-    RPi.GPIO.output(13, 1)
-    time.sleep(0.2)
-    RPi.GPIO.output(13, 0)
+    hour -= 4
+    if hour >= 0:
+        RPi.GPIO.output(12, 1)
+    else:
+        RPi.GPIO.output(12, 0)
 
-    RPi.GPIO.output(15, 1)
-    time.sleep(0.2)
-    RPi.GPIO.output(15, 0)
+    hour -= 8
+    if hour >= 0:
+        RPi.GPIO.output(11, 1)
+    else:
+        RPi.GPIO.output(11, 0)
 
     RPi.GPIO.output(29, 1)
     time.sleep(0.2)
@@ -55,11 +70,3 @@ while True:
         break
 
 RPi.GPIO.cleanup()
-
-
-import datetime
-
-current_date = datetime.datetime.now()
-hour = current_date.hour
-minute = current_date.minute
-print(current_date)
